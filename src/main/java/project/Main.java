eb2a02ff2fde00f782df3c511d1253bd28af317d
helexander <jeremychee97@gmail.com>
2021-12-15 15:54:57 +1100

    Testing on main

---
+++ src/main/java/project/Main.java
@@ -0,0 +1,28 @@
+package project;
+
+public class Main {
+    public static void main(String[] args){
+        enum cardVal {
+            TWO("2", 2);
+
+            final String text;
+            final Integer val;
+
+            cardVal(final String text, Integer val){
+                this.text = text;
+                this.val = val;
+            }
+        }
+
+        String testInput = "4H 4C 6S 7S KD 2C 3S 9S 9D TD";
+
+        // Dividing cards among player 1 and player 2
+        String p1_hand = testInput.substring(0, 14);
+        String p2_hand = testInput.substring(15, 29);
+
+        System.out.printf("Player 1 hand: %s\n", p1_hand);
+        System.out.printf("Player 2 hand: %s", p2_hand);
+
+
+    }
+}
