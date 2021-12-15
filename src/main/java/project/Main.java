ad835f9a64803377fbb332842a0946b0a99e2ba0
helexander <jeremychee97@gmail.com>
2021-12-15 16:36:48 +1100

    valid regex check

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,6 +1,8 @@
 package project;
 
 public class Main {
+    public static final String VALID_PATTERN = "([1-9TKQJA][DHSC]\\s?){10}";
+
     public static void main(String[] args){
         enum cardVal {
             TWO("2", 2);
@@ -13,8 +15,13 @@ public class Main {
                 this.val = val;
             }
         }
-
+        
         String testInput = "4H 4C 6S 7S KD 2C 3S 9S 9D TD";
+
+        // Regex check for input
+        if(!testInput.matches(Main.VALID_PATTERN)){
+           throw new Error("The input was not valid");
+        }
 
         // Dividing cards among player 1 and player 2
         String p1_hand = testInput.substring(0, 14);
