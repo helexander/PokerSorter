77d0d9a7d2358413f27dc7304846f03681ac7abb
helexander <jeremychee97@gmail.com>
2021-12-17 16:58:27 +1100

    refactored junit test for regex check

--- src/test/java/project/MainTest.java
+++ src/test/java/project/MainTest.java
@@ -7,20 +7,18 @@ import static org.junit.Assert.*;
 
 public class MainTest {
     @Test
-    public void test_PokerHandRegexConstructor(){
-        final String VALID_PATTERN = "([1-9TKQJA][DHSC]\\s?){10}";
-
-
+    public void test_PokerHandRegexConstructor() {
         String testInputShouldPass = "4H 4C 6S 7S KD 2C 3S 9S 9D TD";
         String testInputShouldFail = "JS 9S 6S JD 5H 9D K9 8H 5S DD";
 
         String moreThanTenCards = "8H 7D QH 3H QD 3S 4H 2D 7H TD 2C JH QD";
         String equalTenCards = "TH 8H 7S QS 7D 5S 6C 9S 8D 2H";
 
-        Assert.assertEquals(true, testInputShouldPass.matches(VALID_PATTERN));
-        Assert.assertEquals(false, testInputShouldFail.matches(VALID_PATTERN));
+        Assert.assertEquals(true, testInputShouldPass.matches(Main.VALID_PATTERN));
+        Assert.assertEquals(false, testInputShouldFail.matches(Main.VALID_PATTERN));
 
-        Assert.assertEquals(false, moreThanTenCards.matches(VALID_PATTERN));
-        Assert.assertEquals(true, equalTenCards.matches(VALID_PATTERN));
+        Assert.assertEquals(false, moreThanTenCards.matches(Main.VALID_PATTERN));
+        Assert.assertEquals(true, equalTenCards.matches(Main.VALID_PATTERN));
     }
+
 }
